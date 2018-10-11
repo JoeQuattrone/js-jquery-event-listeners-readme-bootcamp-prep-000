c81cd624890abf547b21f3945a724ccf0f060f41
Joe Quattrone <joequattrone5@gmail.com>
2018-10-11 15:12:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 function getIt () {
   $('p').on('click', function() { 
     alert ("Hey!")
-  })};
+  })}
    
    $(window).on('load', function frameIt() {
     $('img').addClass("tasty")
