5fe23f0d242e3fe8d0c80828208c9cca869b92d3
Joe Quattrone <joequattrone5@gmail.com>
2018-10-11 14:45:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
    
    $(window).on('load', function frameIt() {
     $('img').addClass("tasty")
-     $('img').css({ "display": "inline-block" })
-       $('img').css({ "border-color": "red", "border-style": "solid", "border-weight": "5px"}); 
-  }); 
+    $('img').css({ "display": "inline-block" })
+    $('img').css({ "border-color": "red", "border-style": "solid", "border-weight": "5px"}); 
+    }); 
 
 });
